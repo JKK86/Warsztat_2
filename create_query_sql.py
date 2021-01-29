d3a4c2972bb4a2efd20263cf6f358b8c38266b0c
JKK86 <j.kurowski86@wp.pl>
2021-01-29 11:30:55 +0100

    Konfiguracja bazy danych - teraz działa

--- create_query_sql.py
+++ create_query_sql.py
@@ -4,17 +4,18 @@ id serial,
 username varchar(255),
 hashed_password varchar(80),
 PRIMARY KEY (id)
-)"""
+);"""
 
 
 messages_table_query = """
 CREATE TABLE messages(
+id serial,
 from_id integer,
 to_id integer,
 creation_data timestamp,
 PRIMARY KEY (id),
-FOREIGN KEY(from_id) REFERENCES users(id)
+FOREIGN KEY(from_id) REFERENCES users(id),
 FOREIGN KEY(to_id) REFERENCES users(id)
-)"""
+);"""
 
 creation_query_list = [users_table_query, messages_table_query]
